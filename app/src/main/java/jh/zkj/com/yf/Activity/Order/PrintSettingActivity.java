4c6f54fb39e4079367f3041bc2ca14accb3ee330
linyujie <418265421@qq.com>
2018-11-19 18:10:52 +0800

    打印

---
+++ app/src/main/java/jh/zkj/com/yf/Activity/Order/PrintSettingActivity.java
@@ -0,0 +1,18 @@
+package jh.zkj.com.yf.Activity.Order;
+
+import android.os.Bundle;
+
+import jh.zkj.com.yf.Activity.MBaseActivity;
+import jh.zkj.com.yf.R;
+
+/**
+ * Created by linyujie on 18/11/19.
+ */
+
+public class PrintSettingActivity extends MBaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_print_setting);
+    }
+}
